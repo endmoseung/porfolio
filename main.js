7045ff3e0fe0b63900641676c4cfea09d756363c
endmoseung <tmdah900@naver.com>
2022-05-03 11:36:48 +0900

    JS : navbar

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+//Make navbar transparent when it is on the top
+'use strict';
 const toggleBtn = document.querySelector(".navbar__toggle-btn");
 const headboxContent = document.querySelector(".headbox__content");
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 toggleBtn.addEventListener('click',()=>{
     headboxContent.classList.toggle('active');
+});
+
+document.addEventListener("scroll",()=>{
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add("navbar--dark");
+    }else{
+        navbar.classList.remove("navbar--dark");
+    }
 });
