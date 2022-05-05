923c5e042247fbace452411d5a13e2ca2ca741e5
endmoseung <tmdah900@naver.com>
2022-05-05 12:27:44 +0900

    final modified

--- main.js
+++ main.js
@@ -16,52 +16,8 @@ document.addEventListener("scroll",()=>{
         navbar.classList.remove("navbar--dark");
     }
 });
-
-//Handle scrolling when tapping on the navbar menu
-/*
-const homeButton = document.querySelector("#home__button");
-const aboutButton = document.querySelector("#about__button");
-const skillsButton = document.querySelector("#skills__button");
-const myworkButton = document.querySelector("#mywork__button");
-const testimonialButton = document.querySelector("#testimonial__button");
-const contactButton = document.querySelector("#contact__button");
-const contactMeButton = document.querySelector(".contactme");
-const headboxContentButton = document.querySelector(".headbox__content--button");
-
-homeButton.addEventListener("click",()=>{
-  window.scrollTo(0,0);
-  headboxContentButton.classList.remove("active");
-  homeButton.classList.add("active");
-})
-aboutButton.addEventListener("click",()=>{
-  window.scrollTo(0,585);
-  headboxContentButton.classList.remove("active");
-  aboutButton.classList.add("active");
-})
-skillsButton.addEventListener("click",()=>{
-  window.scrollTo(0,1421);
-  headboxContentButton.classList.remove("active");
-  skillsButton.classList.add("active");
-})
-myworkButton.addEventListener("click",()=>{
-  window.scrollTo(0,2173);
-  headboxContentButton.classList.remove("active");
-  myworkButton.classList.add("active");
-})
-testimonialButton.addEventListener("click",()=>{
-  window.scrollTo(0,3287);
-  headboxContentButton.classList.remove("active");
-  testimonialButton.classList.add("active");
-})
-contactButton.addEventListener("click",()=>{
-  window.scrollTo(0,3653.5);
-  headboxContentButton.classList.remove("active");
-  contactButton.classList.add("active");
-})
-contactMeButton.addEventListener("click",()=>{
-  window.scrollTo(0,3653.5);
-})*/
-
+// 냅바 버튼을 클릭하면 해당하는 section으로 이동함
+const headboxContentButtonActive = document.querySelector(".headbox__content--button.active");
 const contactMeButton = document.querySelector(".contactme");
 const headboxContentButton = document.querySelector(".headbox__content--button");
 
@@ -69,6 +25,7 @@ headboxContent.addEventListener("click",(event)=>{
   
   const target = event.target;
   const link = target.dataset.link;
+  console.log(target);
   if(link == undefined){
     return;
   }
@@ -76,23 +33,22 @@ headboxContent.addEventListener("click",(event)=>{
   scrollIntoView(link);
 })
 
+
+
 contactMeButton.addEventListener("click",()=>{
   scrollIntoView('#contact');
-});
 
-function scrollIntoView(selector){
-  const scrollTo = document.querySelector(selector);   //link 변수에 있는 dataset 값을 추출한것이고 여기서는 selector
-  scrollTo.scrollIntoView({behavior : 'smooth'});
-}
+});
 
 //스크롤 했을때 홈이 transparent되게
 
 const homeMom = document.querySelector(".homemom");
-const homeHeight = homeMom.getBoundingClientRect().height;
-
-
+const homemomheight = homeMom.getBoundingClientRect().height;
+const home = document.querySelector("#home");
+const homeheight = home.getBoundingClientRect().height;
+console.log(homeheight);
 document.addEventListener("scroll",()=>{
-  homeMom.style.opacity = 1 - window.scrollY / homeHeight;
+  homeMom.style.opacity = 1 - window.scrollY / homemomheight;
 });
 
 // up button
@@ -102,7 +58,7 @@ document.addEventListener("scroll",()=>{
   function byeButton(){
     arrowBtn.classList.remove("active");
   }
-  if(window.scrollY > homeHeight){
+  if(window.scrollY > homeheight){
     arrowBtn.classList.add("active");
     setTimeout(byeButton,5000)//시간이 일정시간 지났을때 함수호출 여기서는 5000ms 즉 5초뒤에 arrowbtn이 사라지도록 함
   }else{
@@ -146,3 +102,58 @@ setTimeout(()=>{
 },300);
 
 });
+
+////remove active from the previous button's border, and select the new one's border
+//1. 모든 섹션들의 요소들과 메뉴아이템을 가져온다
+//2. intersectionobserver를 통해서 모든 섹션들을 관찰한다
+//3. 보여지는 섹션에 해당하는 버튼을 활성화 시킨다.
+
+const sectionIds = ['#home','#about','#skills','#mywork','#testimonial','#contact']; //애는 걍 단순한 배열
+const sections = sectionIds.map(id=>document.querySelector(id)); //map은 일일히 위에 배열을 돌면서 각각의 배열에 해당하는값에 맞는 것을 queryselector로 불러온것을 배열로 만든것
+const navItems = 
+sectionIds.map(id=>document.querySelector(`[data-link="${id}"]`));//애도 똑같음
+
+let selectedNavItem = navItems[0];
+const observeroption = {
+  root:null,
+  rootmargin: '0px',
+  threshold : 0.3,
+}
+let selectedNavIndex = 0;
+function selectNavItem(selected){
+      selectedNavItem.classList.remove("active");
+      selectedNavItem = selected;
+      selectedNavItem.classList.add("active");
+}
+const observerCallback = (entries, observer)=>{
+  entries.forEach(entry =>{
+    if(!entry.isintersecting && entry.intersectionRatio > 0){// 요소가 빠져나가고 있을때
+      const index = sectionIds.indexOf(`#${entry.target.id}`); //몇번쨰 배열인지 알려준다
+      // 밑에에 대한 설명 스크롤이 아래로 내려가면서 페이지가 올라오는경우
+      if(entry.boundingClientRect.y<0){
+        selectedNavIndex= index + 1;
+      }else{
+        selectedNavIndex= index - 1;
+      }
+    }
+  })
+}
+const observer = new IntersectionObserver(observerCallback, observeroption);
+sections.forEach(section => observer.observe(section)); // sections배열안에 있는 모든것들을 observer해라는 뜻이고 section은 아무거나 들가도 된다
+//화면이 아래로 내려갈떄는 빠져나가는 요소의 0.0의 값이 -이므로 요소의+1값을 불러오고 화면이 위로 올라갈때는 빠져나가는 요소의 0,0의 값이+이므로 요소의-1값을 불러온다 
+window.addEventListener("wheel",()=>{ //사용자가 직접 이동할때= wheel
+  console.log(window.innerHeight);
+  if(window.scrollY === 0){
+    selectedNavIndex=0;
+  }else if (window.scrollY + window.innerHeight === document.body.getBoundingClientRect().height){//이떄까지 내린 scrolly의 값과 현재 윈도우height의 합은 총 페이지의 하잇과 같다
+    selectedNavIndex = navItems.length-1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);   //link 변수에 있는 dataset 값을 추출한것이고 여기서는 selector
+  scrollTo.scrollIntoView({behavior : 'smooth'});
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
